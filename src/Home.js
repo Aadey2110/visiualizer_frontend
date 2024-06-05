298b2b0c475d0cf0f987913c45d9342047334407
aadey shah <aadeyshah55@gmail.com>
2024-06-05 19:39:17 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 export default function Home() {
   const [changes, setChanges] = useState("");
 
-  fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => setChanges(JSON.stringify(response, null, 2)));
+  fetch('https://c761-203-110-85-250.ngrok-free.app/api/changes')
+            .then((response) => response.json())
+            .then(json => setChanges(JSON.stringify(json, null, 2)));
 
   return (<div className="Home">
     <h1>Hello From Home</h1>
