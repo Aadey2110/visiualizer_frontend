5fe6ef75e52d6ab6b3cebeac0fa683aa65282ea4
aadey shah <aadeyshah55@gmail.com>
2024-06-12 02:17:54 +0530

    diff added

--- src/Home.js
+++ src/Home.js
@@ -14,18 +14,88 @@ export default function Home() {
   return (
     <div className="Home">
       <h1 style={{ textAlign: "center" }}>Hello From Home</h1>
+      <div className="main-diff">
+        {changes?.paths &&
+          Object.keys(changes.paths).map((node) => {
+            return (
+              <div
+                className="affeced-node-head"
+                style={{
+                  border: "7px solid black",
+                  padding: "20px 33px",
+                  margin: "40px 0px",
+                }}
+              >
+                <DisplayNode
+                  name={node}
+                  pathsTo={changes.paths[node]}
+                  nodeChanges={changes.changedValues}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
 
-      <pre id="json">{JSON.stringify(changes.paths, null, 2)}</pre>
-      {changes?.changedValues &&
-        Object.keys(changes.changedValues).map((node) => {
-          console.log(node);
+function DisplayNode({ name, pathsTo, nodeChanges }) {
+  return (
+    <div className="node-head">
+      <h3 style={{ fontSize: "35px", fontWeight: "400" }}>Query : {name}</h3>
+      <div className="all-paths">
+        {Object.keys(pathsTo).map((endNode) => {
           return (
-            <DiffView
-              oldText={changes.changedValues[node].oldValue}
-              newText={changes.changedValues[node].newValue}
-            />
+            <div
+              className="nodewise-change"
+              style={{
+                border: "2px solid black",
+                padding: "33px",
+                margin: "40px 0px",
+              }}
+            >
+              <h4 style={{ fontSize: "30px", fontWeight: "400" }}>
+                Affected Fragment : {endNode}
+              </h4>
+              <div>
+                <div style={{ fontSize: "30px", fontWeight: "400" }}>
+                  Possible Paths :
+                </div>
+                <DisplayPaths paths={pathsTo[endNode]} />
+              </div>
+              <div className="diff-display">
+                <DiffView
+                  oldText={nodeChanges[endNode].oldValue}
+                  newText={nodeChanges[endNode].newValue}
+                />
+              </div>
+            </div>
           );
         })}
+      </div>
+    </div>
+  );
+}
+
+function DisplayPaths({ paths }) {
+  return (
+    <div className="paths-head">
+      {paths.map((path) => {
+        return (
+          <p className="path" style={{ margin: "20px 0px", fontSize: "26px" }}>
+            {path.split(" -> ").map((currNode, index) => {
+              return (
+                <span>
+                  {currNode}{" "}
+                  {index !== path.split(" -> ").length - 1 && (
+                    <span> &rarr; </span>
+                  )}{" "}
+                </span>
+              );
+            })}
+          </p>
+        );
+      })}
     </div>
   );
 }
